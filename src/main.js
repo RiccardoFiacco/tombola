b92b04513f388ae64762191651148359c4174b8e
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-02-09 14:54:58 +0100

    add first try to random number choise

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
 import './style.css'
+//caricamento numeri nell'html
+for(let i = 0; i < 90 ; i++){
+    tombolone.innerHTML += 
+    `<div id="${i}" class="bg-slate-600 flex justify-center w-10 rounded-3xl"> ${i} </div>`
+}
+
+function randomNumber(){
+    return Math.floor(Math.random() * 90)
+}
+
+let result = randomNumber()
+
+let domEl = document.getElementById(result)
+domEl.style.backgroundColor = 'green'
