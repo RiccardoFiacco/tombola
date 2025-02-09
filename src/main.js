21a3e6c2c8cf354f4f61821bcf34bae634fbea93
RiccardoFiacco <riccardofiacco17@gmail.com>
2025-02-10 00:05:36 +0100

    add logic to chose a number

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import './style.css'
 //caricamento numeri nell'html
 for(let i = 0; i < 90 ; i++){
     tombolone.innerHTML += 
-    `<div id="${i}" class="bg-slate-600 flex justify-center w-10 rounded-3xl"> ${i} </div>`
+    `<div id="${i}" class="flex justify-center w-10 rounded-3xl"> ${i} </div>`
 }
 
 function randomNumber(){
     return Math.floor(Math.random() * 90)
 }
 
-let result = randomNumber()
+estrai.addEventListener('click', () => {
+    let result = randomNumber()
+    document.getElementById(result).style.backgroundColor = 'green'
+})
 
-let domEl = document.getElementById(result)
-domEl.style.backgroundColor = 'green'
+
